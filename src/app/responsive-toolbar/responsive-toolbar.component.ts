0d17861f9bd6d6d55fceae4533c3167231031462
vidumini <heartyvds@gmail.com>
2020-08-12 19:35:06 +0530

    added responsive toolbar

---
+++ src/app/responsive-toolbar/responsive-toolbar.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item';
+
+@Component({
+  selector: 'app-responsive-toolbar',
+  templateUrl: './responsive-toolbar.component.html',
+  styleUrls: ['./responsive-toolbar.component.css']
+})
+export class ResponsiveToolbarComponent implements OnInit {
+  
+  menuItems: MenuItem[] = [
+    {
+      label: 'Sign Up',
+      icon: 'login'
+    },
+    {
+      label: 'About',
+      icon: 'help'
+    },
+    {
+      label: 'Pricing',
+      icon: 'attach_money'
+    },
+    {
+      label: 'Docs',
+      icon: 'notes'
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow'
+    },
+    {
+      label: 'Blog',
+      icon: 'rss_feed'
+    },
+
+  
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
