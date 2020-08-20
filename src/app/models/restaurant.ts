051f36a99c03bba19093924bda95f22b80f85ff0
vidumini <heartyvds@gmail.com>
2020-08-20 18:59:04 +0530

    updated restaurant model

--- src/app/models/restaurant.ts
+++ src/app/models/restaurant.ts
@@ -1,7 +1,17 @@
 export class Restaurant
 {
-  constructor(public id, public name: string, public address: string)
-  {
+  private name: string;
+  private address: string
+  private phone_number: string
+  private longitude: string
+  private latitude: string  
 
+  constructor(name: string, address: string, phone_number: string, longitude: string, latitude: string)
+  {
+    this.name = name;
+    this.address = address;
+    this.phone_number = phone_number;
+    this.longitude = longitude;
+    this.latitude = latitude;  
   }
 }
