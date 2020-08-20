e64ed1eaeaf2c1d618360d61dea1cae631d9aa3f
vidumini <heartyvds@gmail.com>
2020-08-20 18:59:56 +0530

    added map

--- src/app/register-restaurant/register-restaurant.component.ts
+++ src/app/register-restaurant/register-restaurant.component.ts
@@ -5,6 +5,14 @@ import { Restaurant } from '../models/restaurant';
 import { Observable } from 'rxjs';
 import { from } from 'rxjs';
 import { map,catchError } from 'rxjs/operators'; 
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
 
 @Component({
   selector: 'app-register-restaurant',
@@ -22,32 +30,61 @@ export class RegisterRestaurantComponent implements OnInit {
   // rest_longitude: string;
   // rest_latitude: string;
   // rest_pnumber: string;
-
+  map: Map
 
   constructor(private api_service: ApiService) { }
 
   ngOnInit(): void {
   	// this.restaurants$ = this.api_service.get_restaurants();
+    this.map = new Map({
+      target: 'hotel_map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([6.203364, 80.138088]),
+        zoom: 2
+      })
+    });  	
   }
 
-  model = new Restaurant(43, 'Ravino', 'Unawatuna');
+  model = new Restaurant('Raveena', 'Unawatuna', '0770273653', '23', '77');
+
+  
 
   onSubmit(form: NgForm)
   {
+    console.log(form.value.user_role);
+
     const data = {
     	data:{
     		mdata:{
 			    user: "vidumini",    
-			  	name:"Raveena",
-			    address: "Unawatuna",
-			    phone_number: "0770273653",
+			  	name:form.value.rest_name,
+			    address: form.value.rest_address,
+			    phone_number: form.value.rest_pnumber,
 			    longitude: "23.5444",
 			    latitude: "77.5444",
-			    role: "customer"
+			    role: form.value.user_role
     		}
     	}
     };
-    // this.restaurants$ = this.api_service.get_restaurants();
+
+    // const ndata = JSON.stringify(this.model);
+
+    // try {
+    //     JSON.parse(ndata);
+    // } catch (e) {
+    //     console.log("False")
+    // }
+    // console.log(this.model)
+    // console.log(ndata)
+    // console.log(typeof(this.model))
+    // console.log(typeof(JSON.stringify(this.model)))
+
+    //this.restaurants$ = this.api_service.get_restaurants();
     this.api_service.register_restaurant2(data).pipe(
         map(resp => resp),
         catchError(err => {
@@ -58,8 +95,9 @@ export class RegisterRestaurantComponent implements OnInit {
       resp => console.log(resp),
       err => console.log(err)
     );
-    // console.log(this.restaurants$)
+
   }
 
 
 }
+
