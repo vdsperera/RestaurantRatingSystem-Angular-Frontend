dde683473e3dc99b7e692aa04015bcbcd2710023
vidumini <heartyvds@gmail.com>
2020-08-21 15:18:23 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RestaurantListComponent},
+  {path: 'registerrestaurant', component: RegisterRestaurantComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
