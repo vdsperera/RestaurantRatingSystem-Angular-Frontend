5123d5c0d23ae61193808cf702620eb6df5417c6
vidumini <heartyvds@gmail.com>
2020-08-31 18:55:42 +0530

    removed start rating codes from restaurant details component

--- src/app/restaurant-details/restaurant-details.component.ts
+++ src/app/restaurant-details/restaurant-details.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-restaurant-details',
@@ -9,55 +8,15 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class RestaurantDetailsComponent implements OnInit {
 
 
-  @Input('rating') public rating: number;
-  @Input('star_count') public star_count: number=5;
-  @Input('color') public color: string;
-  @Output('rating_updated') public rating_updated = new EventEmitter();
-
   public snack_bar_duration: number = 2000;
   public rating_array = [];
 
 
-  constructor(private snack_bar: MatSnackBar) { }
+  constructor() { }
 
   ngOnInit(): void {
-    for(let index=0; index<this.star_count; index++)
-    {
-      this.rating_array.push(index);
-    }
-  }
-
-  show_icon(index: number)
-  {
-    if(this.rating <= index+1)
-    {
-      return 'note';
-    }
-    else
-    {
-      return 'star_border';
-    }
 
   }
 
-  onClick(rating:number) {
-    console.log(rating)
-    this.snack_bar.open('You rated ' + rating + ' / ' + this.star_count, '', {
-      duration: this.snack_bar_duration
-    });
-    this.rating_updated.emit(rating);
-    return false;
-  }  
-
-
-
-}
-
-
-export enum star_rating_color
-{
-  primary = 'primary',
-  accent = 'accent',
-  warn = 'warn'
 
 }
