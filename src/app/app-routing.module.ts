f15d2e080a9ee199fe527d101876ecde58709be2
vidumini <heartyvds@gmail.com>
2020-10-06 18:36:09 +0530

    set routing for restaurant details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
 
 const routes: Routes = [
   {path: '', component: RestaurantListComponent},
-  {path: 'registerrestaurant', component: RegisterRestaurantComponent}
+  {path: 'registerrestaurant', component: RegisterRestaurantComponent},
+  {path: 'restaurant/:id', component: RestaurantDetailsComponent},
 ];
 
 @NgModule({
