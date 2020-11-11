453f2e1921c6d18aca9375b2a321d764ddb93707
vidumini <heartyvds@gmail.com>
2020-11-11 15:01:09 +0530

    added restaurant component method for adding ratings(test)

--- src/app/restaurant-details/restaurant-details.component.ts
+++ src/app/restaurant-details/restaurant-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import { Rating } from '../models/rating';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service' 
+import { map,catchError } from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-restaurant-details',
@@ -12,9 +15,10 @@ export class RestaurantDetailsComponent implements OnInit {
   public snack_bar_duration: number = 2000;
   public rating_array = [];
   public rating_model = new Rating;
+  
 
-
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private api_service: ApiService) { }
 
   ngOnInit(): void {
 
@@ -24,8 +28,46 @@ export class RestaurantDetailsComponent implements OnInit {
   {
     this.rating_model = $event;
     console.log('even emitter');
-    console.log(this.rating_model);
+    
   }
+
+  submit()
+  {
+    
+    let rating_data = this.rating_model.get_ratings()
+    let value = (<HTMLSelectElement>document.getElementById('dishesdrop')).value;
+    let restaurant_id = this.route.snapshot.paramMap.get('id')
+    // console.log(rating_data.price_rating);
+    const data = {
+      data:{
+        mdata:{
+          "user": "vidumini",
+          "token_number": null,
+          "restaurant_id": +restaurant_id,
+          "dish_id": value,
+          "dish_rating": rating_data.dish_rating,
+          "price_rating": rating_data.price_rating,
+          "service_rating": rating_data.service_rating, 
+          "review": "Good experience"
+        }
+      }
+    };
+    console.log(data);
+    console.log('route param ' + this.route.snapshot.paramMap.get('id'))
+  
+  
+    this.api_service.add_rating(data).pipe(
+        map(resp => resp),
+        catchError(err => {
+          throw err;
+        })
+    )
+    .subscribe(
+      resp => console.log(resp),
+      err => console.log(err)
+    );
+  }
+
 
 
 }
