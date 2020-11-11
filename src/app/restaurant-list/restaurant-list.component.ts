23eab89ec06091d8f5e1cf7aefb8e6bd39bf404a
vidumini <heartyvds@gmail.com>
2020-11-11 15:02:10 +0530

    updated dish rating functionality in restaurant list component(test)

--- src/app/restaurant-list/restaurant-list.component.ts
+++ src/app/restaurant-list/restaurant-list.component.ts
@@ -20,34 +20,43 @@ export class RestaurantListComponent implements OnInit {
 
   ngOnInit(): void {
     this.get_restaurants();
+
   }
 
   public get_restaurants() {
-    // this.restaurants$ = this.apiService.get_restaurants();
-    this.apiService.get_restaurants()
-    .subscribe((data) => {
-       console.log('start component console restaurants')
-       // this.restaurants$ = data['data']['restaurant_list']
-       this.restaurants = data['data']['restaurant_list']
-       // this.rests = data['data']['restaurant_list']
-       // // console.log(this.rests)
-       
-       // // this.rests.sort(function(a, b){return a.restaurant_id - b.restaurant_id});
-       // this.restaurants.sort(function(a, b){return b.overall_rating - a.overall_rating});
+    let value = (<HTMLSelectElement>document.getElementById('dishesdrop')).value;
+    if(value != 'none')
+      this.get_restaurants_dish(value)
+    else
+    {
+      // this.restaurants$ = this.apiService.get_restaurants();
+      this.apiService.get_restaurants()
+      .subscribe((data) => {
+         console.log('start component console restaurants')
+         // this.restaurants$ = data['data']['restaurant_list']
+         this.restaurants = data['data']['restaurant_list']
+         // this.rests = data['data']['restaurant_list']
+         // // console.log(this.rests)
+         
+         // // this.rests.sort(function(a, b){return a.restaurant_id - b.restaurant_id});
+         // this.restaurants.sort(function(a, b){return b.overall_rating - a.overall_rating});
 
-       console.log(this.restaurants)
-       console.log('end component console restaurants')
-    });
-    // console.log('start component console')
-    // console.log(this.rests)
-    // console.log(this.restaurants)
-    // console.log('end component console')
+         console.log(this.restaurants)
+         console.log('end component console restaurants')
+      });
+      // console.log('start component console')
+      // console.log(this.rests)
+      // console.log(this.restaurants)
+      // console.log('end component console')
+    }
 
   }
 
-  public get_restaurants_dish()
+  public get_restaurants_dish(value)
   {
-    this.apiService.get_restaurants_dish(3)
+    // let value = (<HTMLSelectElement>document.getElementById('dishesdrop')).value;
+    console.log('selected one is ' + value)
+    this.apiService.get_restaurants_dish(value)
     .subscribe((data) => {
        console.log('start component console restaurant dish')
        // this.restaurants$ = data['data']['restaurant_list']
