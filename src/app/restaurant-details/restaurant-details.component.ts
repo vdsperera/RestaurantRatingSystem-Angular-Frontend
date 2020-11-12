bd87a7f16b66eea8b8b611f79b0f4fff5c1a4bff
vidumini <heartyvds@gmail.com>
2020-11-12 23:26:46 +0530

    added component functions for getting dish rating list for a rest(test)

--- src/app/restaurant-details/restaurant-details.component.ts
+++ src/app/restaurant-details/restaurant-details.component.ts
@@ -3,6 +3,7 @@ import { Rating } from '../models/rating';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api.service' 
 import { map,catchError } from 'rxjs/operators'; 
+import { DishRating } from '../dish_rating';
 
 @Component({
   selector: 'app-restaurant-details',
@@ -15,13 +16,16 @@ export class RestaurantDetailsComponent implements OnInit {
   public snack_bar_duration: number = 2000;
   public rating_array = [];
   public rating_model = new Rating;
+  public dish_rating_list;
+  private star;
+  private checked_star;
   
 
   constructor(private route: ActivatedRoute,
     private api_service: ApiService) { }
 
   ngOnInit(): void {
-
+    this.get_dish_rating_list(this.route.snapshot.paramMap.get('id'))
   }
 
   rating_event_hander($event: any)
@@ -66,6 +70,31 @@ export class RestaurantDetailsComponent implements OnInit {
       resp => console.log(resp),
       err => console.log(err)
     );
+  }
+
+  get_dish_rating_list(restaurant_id)
+  {
+   this.api_service.get_dish_rating_list_for_restaurant(restaurant_id)
+   .subscribe((data) => {
+     this.dish_rating_list = data['data']['dish_ratings']
+     console.log(this.dish_rating_list)
+   }); 
+  }
+
+  set_star_rating(x)
+  {
+    this.checked_star = Math.floor(x);
+    this.star = (5 - Math.floor(x))
+  }
+
+  checked_star_array(x)
+  {
+    return Array(this.checked_star)
+  }
+
+  star_array()
+  {
+    return Array(this.star)
   }
 
 
