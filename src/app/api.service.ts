6f87658702d4a6c80f722b258dc07afdf701b63b
vidumini <heartyvds@gmail.com>
2020-11-14 21:34:57 +0530

    set api service for sample data and backend data

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,6 +11,7 @@ import { map } from 'rxjs/operators';
 export class ApiService {
 
   API_URL = 'http://127.0.0.1:8000';
+  type = 0
 
   constructor(private http: HttpClient) { }
 
@@ -61,14 +62,17 @@ export class ApiService {
     // });
     // console.log('end api service')
     // return this.http.get<Restaurant[]>(`${this.API_URL}/restaurants/list?dishid=${dish_id}`);
-    if(dish_id==1)
-        return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_1.json`);
-    if(dish_id==2)
-        return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_2.json`);
-    if(dish_id==3)
-        return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_3.json`);
-    if(dish_id==4)
-        return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_4.json`);                   
+    if(this.type == 0)
+        return this.http.get<Restaurant[]>(`${this.API_URL}/restaurants/list?dishid=${dish_id}`);
+    if(this.type == 1)
+        if(dish_id==1)
+            return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_1.json`);
+        if(dish_id==2)
+            return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_2.json`);
+        if(dish_id==3)
+            return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_3.json`);
+        if(dish_id==4)
+            return this.http.get<Restaurant[]>(`/assets/sample_responses/get_restaurant_list_for_dish_n/get_restaurant_list_for_dish_4.json`);                   
   }
 
   public register_restaurant(restaurant: Restaurant):Observable<Restaurant>
@@ -96,10 +100,14 @@ export class ApiService {
     //    // console.log(data)
     //    // console.log('end api service console')
     // });
-    if(restaurant_id==22)
-        return this.http.get<Restaurant[]>(`/assets/sample_responses/get_dish_list_for_restaurant_n/get_dish_list_for_restaurant_22.json`);
-    if(restaurant_id==23)
-        return this.http.get<Restaurant[]>(`/assets/sample_responses/get_dish_list_for_restaurant_n/get_dish_list_for_restaurant_23.json`);
+    if(this.type == 0)
+        return this.http.get<any[]>(`${this.API_URL}/ratings/dishes/list?restid=${restaurant_id}`)
+    if(this.type == 1)
+      if(restaurant_id==22)
+          return this.http.get<Restaurant[]>(`/assets/sample_responses/get_dish_list_for_restaurant_n/get_dish_list_for_restaurant_22.json`);
+      if(restaurant_id==23)
+          return this.http.get<Restaurant[]>(`/assets/sample_responses/get_dish_list_for_restaurant_n/get_dish_list_for_restaurant_23.json`);
+
   }
 
 }
