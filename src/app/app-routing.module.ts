72c9aa9db1ec48730667a1686e61698eba27ece1
vidumini <heartyvds@gmail.com>
2020-12-19 19:08:11 +0530

    set routing for getting contributor list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
+import { ContributorsComponent } from './contributors/contributors.component'
 
 const routes: Routes = [
   {path: '', component: RestaurantListComponent},
   {path: 'registerrestaurant', component: RegisterRestaurantComponent},
   {path: 'restaurants/:id', component: RestaurantDetailsComponent},
+  {path: 'contributors', component: ContributorsComponent}
 ];
 
 @NgModule({
