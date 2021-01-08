d22760b729a246cb04a665db3363e4d286e0fbe6
vidumini <heartyvds@gmail.com>
2021-01-08 18:33:43 +0530

    changed search term as restaurant name

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -6,13 +6,14 @@ export class FilterPipe implements PipeTransform
   transform(items: any[], search_text: string): any[]
   {
   	if(!items) return [];
+    console.log(items)
   	if(!search_text) return items;
 
   	search_text = search_text.toLocaleLowerCase()
 
   	return items.filter((it) => {
-  		console.log(it.restaurant_name)
-  		return it.address.toLocaleLowerCase().includes(search_text)
+  		// console.log(it.restaurant_name)
+  		return it.restaurant_name.toLocaleLowerCase().includes(search_text)
   	})
   }
 
